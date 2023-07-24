0b7817f9fcfc2ac170badb6d2037a40af245a7c3
Marcelo Oliveira <119261912+marcelomatheus@users.noreply.github.com>
2023-07-24 20:55:00 -0300

    Update main.cpp
    novo código com fórmula

--- main.cpp
+++ main.cpp
@@ -4,44 +4,49 @@
 #include <string>
 #include <algorithm>
 
-/*
+
 using namespace std;
 
 int main()
 {
     vector<int> homens;
     vector<int>::iterator it;
-    int qHomens, repeticoes, salto, distancia;
+    int qHomens, repeticoes, k, distancia, p = 0;
 
     cin >> repeticoes;
     for(int j = 0; j < repeticoes; j++){
-    cin >> qHomens;
-    cin >> salto;
+        cin >> qHomens;
+        cin >> k;
 
-    for(int i = 1; i <= qHomens; i++){
-        homens.push_back(i);
-    }
-
-    it = homens.begin()+salto-1;
-    homens.erase(it);
-    it = it+1;
-
-    while(homens.size()!=1){
-    if(salto<=distance(it,homens.end())){
-        advance(it,salto-1);
-        if(*it==*homens.end()){
-            homens.erase(it);
-            it=homens.begin()+salto;
+        for(int i = 1; i <= qHomens; i++){
+            homens.push_back(i);
         }
-        homens.erase(it);
-    }else if(salto>distance(it,homens.end())){
-        it = homens.begin()+distance(it,homens.end());
-        homens.erase(it);
-        }
-    }
-    cout << "case " << j+1 << ": " << homens.front() << endl;
-    homens.clear();
 
-}
+        while(homens.size()!=1){
+            if(k<distance(it,homens.end())){
+                p = p + (k-1);
+                for(int i = 0; i<homens.size(); i++){
+                    cout << homens[i] << " ";
+                    it = homens.begin()+p;
+                    homens.erase(it);
+                }
+                cout << endl;
+            }else if(k==distance(it,homens.end())){
+                p = k;
+                it = homens.begin()+p;
+                homens.erase(it);
+                it = homens.end();
+            }else{
+                p = p % homens.size();
+                for(int i = 0; i<homens.size(); i++){
+                    cout << homens[i] << " ";
+                }
+                it = homens.begin()+p;
+                homens.erase(it);
+            }
+        }
+        cout << "case " << j+1 << ": " << homens.front() << endl;
+        homens.clear();
+    }
     return 0;
-}*/
+}
